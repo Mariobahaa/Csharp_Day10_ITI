c834246aab02af45043d9474034059920d9dc601
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-01 14:57:00 +0200

    LINQ - Element Operators

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -14,7 +14,7 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("P1");
+            Console.WriteLine("LINQ - Restriction Operators");
             //1- Products out of stock
             var res = ProductList.Where(P => P.UnitsInStock == 0);
             Console.WriteLine("1-Products out of Stock:");
@@ -45,6 +45,24 @@ namespace Problem1
             }
             Console.WriteLine("\n");
 
+            Console.WriteLine("LINQ - Element Operators");
+
+            var first = ProductList.First(P => P.UnitsInStock == 0);
+            Console.WriteLine("1- first Product out of stock");
+            Console.WriteLine($"{first}");
+            Console.WriteLine("\n");
+
+            first = ProductList.FirstOrDefault(P => P.UnitPrice >1000);
+            Console.WriteLine("2- first product whose Price > 1000");
+            if (first == null) Console.WriteLine("Not Found");
+            else Console.WriteLine($"{first}");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("3- second number greater than 5");
+            int[] Arr2 = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var gn = Arr2.Where(D => D > 5).ElementAt(1);
+            Console.WriteLine($"{gn}");
+            Console.WriteLine("\n");
         }
     }
 }
