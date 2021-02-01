cdeae5d0db849a775b3d7595493cbbfd06fce275
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-01 13:48:02 +0200

    LINQ - Restriction Operators

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -5,11 +5,46 @@ using static L2O___D09.ListGenerators;
 
 namespace Problem1
 {
+
+    /*
+      CustomerList;
+      ProductList;
+      */
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("P1");
+            //1- Products out of stock
+            var res = ProductList.Where(P => P.UnitsInStock == 0);
+            Console.WriteLine("1-Products out of Stock:");
+            foreach (var p in res)
+            {
+                Console.WriteLine($"{p}");
+            }
+            Console.WriteLine("\n");
+
+            //2- Products out of stock and cost more than 3 per unit
+            res = ProductList.Where(P => P.UnitsInStock == 0 && P.UnitPrice>3.00m);
+            Console.WriteLine("2- Products out of stock and cost more than 3 per unit");
+            foreach (var p in res)
+            {
+                Console.WriteLine($"{p}");
+            }
+            Console.WriteLine("\n");
+
+            //3- Names of digits whose name is shorter than their values
+            string[] Arr = 
+            { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            var dig = Arr.Where((D,i)=> D.Length <i);
+            Console.WriteLine("3- Names of digits whose name is shorter than their values");
+            foreach (var d in dig)
+            {
+                Console.Write($"{d}, ");
+            }
+            Console.WriteLine("\n");
+
         }
     }
 }
