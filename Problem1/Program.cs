3629b7944396b4302ea82b4844c8a5f5b8de8331
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-01 15:44:53 +0200

    LINQ - Partitioning Operators

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -25,7 +25,7 @@ namespace Problem1
             Console.WriteLine("\n");
 
             //2- Products out of stock and cost more than 3 per unit
-            res = ProductList.Where(P => P.UnitsInStock == 0 && P.UnitPrice>3.00m);
+            res = ProductList.Where(P => P.UnitsInStock > 0 && P.UnitPrice>3.00m);
             Console.WriteLine("2- Products out of stock and cost more than 3 per unit");
             foreach (var p in res)
             {
@@ -45,6 +45,7 @@ namespace Problem1
             }
             Console.WriteLine("\n");
 
+            /*-------------------------------------------------------------------*/
             Console.WriteLine("LINQ - Element Operators");
 
             var first = ProductList.First(P => P.UnitsInStock == 0);
@@ -63,6 +64,68 @@ namespace Problem1
             var gn = Arr2.Where(D => D > 5).ElementAt(1);
             Console.WriteLine($"{gn}");
             Console.WriteLine("\n");
+
+
+            /*-------------------------------------------------------------------*/
+            Console.WriteLine("LINQ - Partitioning Operators");
+            //1- 3 orders from customers in Washington
+            var cw = CustomerList.Where(C => C.City == "Washington").Take(3);
+            Console.WriteLine("1- 3 orders from customers in Washington");
+            foreach (var c in cw)
+            {
+                Console.WriteLine($"{c}");
+            }
+            Console.WriteLine("\n");
+
+            //2- all but the first 2 orders from customers in Washington
+            cw = CustomerList.Where(C => C.City == "Washington").Skip(2);
+            Console.WriteLine("2- all but the first 2 orders from customers in Washington");
+            foreach (var c in cw)
+            {
+                Console.WriteLine($"{c}");
+            }
+            Console.WriteLine("\n");
+
+            //3- elements starting from the beginning 
+            //of the array until a number is hit that is less than its position in the array.
+
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var nums = numbers.TakeWhile((N,i) => N>=i);
+            Console.WriteLine("3-numbers until a number is hit that is less than its position in the array");
+            foreach (var n in nums)
+            {
+                Console.Write($"{n}, ");
+            }
+            Console.WriteLine("\n");
+
+
+            //4- elements of the array starting from the first element divisible by 3.
+
+
+            nums = numbers.SkipWhile(N => N%3!=0);
+            Console.WriteLine("4- elements of the array starting from the first element divisible by 3");
+            foreach (var n in nums)
+            {
+                Console.Write($"{n}, ");
+            }
+            Console.WriteLine("\n");
+
+
+            //5- elements of the array starting from the first element less than its position.
+
+
+            nums = numbers.SkipWhile((N,i) => N>=i);
+            Console.WriteLine("5- elements of the array starting from the first element less than its position.");
+            foreach (var n in nums)
+            {
+                Console.Write($"{n}, ");
+            }
+            Console.WriteLine("\n");
+
+
         }
+
+
+
     }
 }
