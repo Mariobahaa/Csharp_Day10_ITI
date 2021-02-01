27a9ed59cf6078167ffe33516e3bf1bb306aa140
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-01 13:17:10 +0200

    Initial commit

---
+++ Problem1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static L2O___D09.ListGenerators;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
